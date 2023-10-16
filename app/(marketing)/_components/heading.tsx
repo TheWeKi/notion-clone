ccc1e54b54c1a85fe5897f1b233428210cd7ef84
Rituraj Pandey <rp.weki.24@gmail.com>
2023-10-16 16:58:29 +0530

    Created Notion Clone & Made Landing Page with Dark Mode Toggle

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export const Heading = () => {
+    return (
+        <div className="max-w-3xl space-y-4">
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+                Your Ideas, Documents & Plans. Unified. Welcome to <span className="underline">
+                    Jotion
+                </span>
+            </h1>
+            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+                Jotion is the connected workspace where teams get work done. From
+                brainstorming ideas to working on documents, organizing plans, and
+                sharing feedback, Jotion makes collaboration seamless.
+            </h3>
+            
+            <Button>
+                Enter Jotion
+                <ArrowRight className="h-4 w-4 ml-2"/>    
+            </Button>
+        </div>
+    );
+}
