001f22101a43cbc8380b79560a6368d6f2652c2a
Rituraj Pandey <rp.weki.24@gmail.com>
2023-10-17 12:34:52 +0530

    Added Convex and Clerk Auth and Database Setup

---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const Navigation = () => {
+    return (
+        <>
+            <aside>
+                navigation menu
+            </aside>
+        </>
+    )
+}
--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,8 +2,15 @@
 
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading = () => {
+
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -16,11 +23,27 @@ export const Heading = () => {
                 brainstorming ideas to working on documents, organizing plans, and
                 sharing feedback, Jotion makes collaboration seamless.
             </h3>
-            
-            <Button>
-                Enter Jotion
-                <ArrowRight className="h-4 w-4 ml-2"/>    
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Enter Jotion
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Jotion Free!
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     );
 }
